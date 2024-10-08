d440e5061107baaed40eb7cdb9827913af6de26e
Pijukatel <Pijukatel@users.noreply.github.com>
2024-10-08 08:38:32 +0200

    Poetry basic project structure

---
+++ src/ref_server.py
@@ -0,0 +1,49 @@
+"""Reference server.
+
+This server is used to test the client implementation.
+
+```shell
+pip install litestar==2.12.1 Faker==29.0.0
+uvicorn ref_server:app
+```
+
+Schema is available at http://127.0.0.1:8000/schema (or http://127.0.0.1:8000/schema/swager).
+"""
+
+import asyncio
+from datetime import date, datetime, timedelta
+from random import choice, randint, random
+from typing import Any
+
+from faker import Faker
+from litestar import Litestar, get
+
+faker = Faker()
+
+
+COUTRIES = [faker.country() for _ in range(5)]
+CITIES = {i: {"name": faker.city(), "country": choice(COUTRIES)} for i in range(30)}
+
+
+@get("/cities")
+async def get_cities() -> list[dict[str, str | int]]:
+    return [{"id": city_id, **city_info} for city_id, city_info in CITIES.items()]
+
+
+@get("/cities/{city_id:int}/stats", cache=True)
+async def get_city_stats(city_id: int, date: date) -> list[dict[str, Any]]:
+    await asyncio.sleep(random() * 4.2)
+    dt = datetime.combine(date, datetime.min.time())
+    return [
+        {
+            "departure-time": dt + timedelta(minutes=randint(0, 720)),
+            "bus-type": f"BUS-{randint(100, 113)}",
+            "passengers": randint(5, 100),
+            "delay": timedelta(minutes=randint(0, 90)),
+            "accident": random() > 0.9,
+        }
+        for _ in range(randint(1000, 3000))
+    ]
+
+
+app = Litestar([get_cities, get_city_stats])
